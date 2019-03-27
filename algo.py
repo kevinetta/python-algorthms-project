1b149c91a480d7704111444a78840591dd3596fc
kevinetta <kevin.etta@marquette.edu>
2019-03-27 18:53:00 -0500

    added longest common substring

--- algo.py
+++ algo.py
@@ -1,5 +1,37 @@
 # algo.py contains the implementation of all the algorithms used in this project
 
+# Longest Common substring
+def longest_common_substring(X, Y, i, j, count) :  
+      
+    if (i == 0 or j == 0) :  
+        return count  
+          
+    if (X[i - 1] == Y[j - 1]) : 
+        count = longest_common_substring(X, Y, i - 1, j - 1, count + 1)  
+      
+    count = max(count, max(longest_common_substring(X, Y, i, j - 1, 0),  
+                           longest_common_substring(X, Y, i - 1, j, 0)))  
+  
+    return count 
+
+# Dynamic solution to longest common substring
+def LCSubStr(X, Y, m, n): 
+  
+	LCSuff = [[0 for k in range(n+1)] for l in range(m+1)] 
+ 
+	result = 0 
+
+	for i in range(m + 1): 
+		for j in range(n + 1): 
+			if (i == 0 or j == 0): 
+				LCSuff[i][j] = 0
+			elif (X[i-1] == Y[j-1]): 
+				LCSuff[i][j] = LCSuff[i-1][j-1] + 1
+				result = max(result, LCSuff[i][j]) 
+			else: 
+				LCSuff[i][j] = 0
+	return result 
+	
 # Longest Common Subsequence
 def lcs(X, Y, m, n): 
   
